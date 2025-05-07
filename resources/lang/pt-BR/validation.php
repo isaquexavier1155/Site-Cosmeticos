bd00c1154533ddfc1c464265f88007dfb0c9716a
isaquexavier1155 <isaque.ixs@gmail.com>
2025-05-06 21:43:47 -0300

    Ajuste idioma de mensagens de erro 2

---
+++ resources/lang/pt-BR/validation.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'required' => 'O campo :attribute é obrigatório.',
+    'min' => [
+        'string' => 'O campo :attribute deve ter no mínimo :min caracteres.',
+    ],
+    'max' => [
+        'string' => 'O campo :attribute não pode ter mais que :max caracteres.',
+    ],
+    'confirmed' => 'A confirmação de :attribute não confere.',
+    
+    'attributes' => [
+        'email' => 'e-mail',
+        'password' => 'senha',
+        'name' => 'nome',
+    ],
+];
