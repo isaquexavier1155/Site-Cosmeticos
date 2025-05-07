5dfad5e487dd66d650b4ab35b48a3de7b6368cfd
isaquexavier1155 <isaque.ixs@gmail.com>
2025-05-06 21:40:53 -0300

    Ajuste idioma de mensagens de erro

--- config/app.php
+++ config/app.php
@@ -78,7 +78,9 @@ return [
     |
     */
 
-    'locale' => env('APP_LOCALE', 'en'),
+    //'locale' => env('APP_LOCALE', 'en'), Alterado para Portugues
+    'locale' => env('APP_LOCALE', 'pt-BR'),
+
 
     'fallback_locale' => env('APP_FALLBACK_LOCALE', 'en'),
 
